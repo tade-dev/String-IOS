0a402e3e65ff64142fbba78ed6e26bb23c8c0043
Akintade Oluwaseun <akintadeseun816@gmail.com>
2025-12-27 23:12:05 +0000

    Initial Commit

---
+++ String-IOS/String_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  String_IOSApp.swift
+//  String-IOS
+//
+//  Created by BSTAR on 27/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct String_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
